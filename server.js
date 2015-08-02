58d1a8c48e463cf34425be16dddca356a4839e8d
venerati <jamesomarshall@gmail.com>
2015-08-02 12:03:20 -0400

    server.js will now respond with index.html to any base user request.

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
-function onRequest(request, response) {
-	console.log('the user has made a request' + request.url);
-	response.writeHead(200, {"context-Type": "text/plain"});
-	response.write("here is some data");
+//404 response
+function send404Res(response) {
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("Error 404: Page not found");
 	response.end();
+}
+
+//handles the user request
+function onRequest(request, response) {
+
+	if(request.method == 'GET' && request.url == '/') {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	}else{
+		send404Res(response);
+	}
 };
 
-http.createServer(onRequest).listen(8765);
+http.createServer(onRequest).listen(8766);
 console.log('Server is active');
